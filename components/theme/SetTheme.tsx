88944fce7baa777484cc990bda51bea88d3447f7
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-10-03 17:05:28 +0530

    added darkmode button successfully

---
+++ components/theme/SetTheme.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+import { DropdownMenuContent, DropdownMenuItem } from "./dropdown-menu";
+import { useTheme } from "next-themes";
+
+const SetTheme = () => {
+  const { setTheme } = useTheme();
+  return (
+    <DropdownMenuContent align="end">
+      <DropdownMenuItem onClick={() => setTheme("light")}>
+        Light
+      </DropdownMenuItem>
+      <DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
+      <DropdownMenuItem onClick={() => setTheme("system")}>
+        System
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  );
+};
+
+export default SetTheme;
