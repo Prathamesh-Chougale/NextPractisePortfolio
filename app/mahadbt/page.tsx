3185edc26d33354f9184a2d12fb5a2ada90dd981
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-12-21 15:57:05 +0530

    Add YouTube video player and check headphone connection

---
+++ app/mahadbt/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Mahadbt = () => {
+  return <div>Mahadbt</div>;
+};
+
+export default Mahadbt;
--- app/sanitytest/page.tsx
+++ app/sanitytest/page.tsx
@@ -8,6 +8,30 @@ const sanity = async () => {
       {projects.map((p: Resource) => (
         <div key={p._id}>{p.title}</div>
       ))}
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/unxAnJBy12Q?si=2OwalLE8XdRvWDUO"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+      {/* check whether headphone is connected or not */}
+
+      {/* {(global.navigator.mediaDevices as any)
+        .getAudioTracks()
+        .some(
+          (track: MediaStreamTrack) =>
+            track.kind === "audiooutput" &&
+            (track.getSettings() as any).deviceId?.includes("headphones")
+        ) ? (
+        // Headphones are connected
+        <div>Headphones are connected</div>
+      ) : (
+        // Headphones are not connected
+        <div>Headphones are not connected</div>
+      )} */}
     </div>
   );
 };
